6fbc56055c51a63a789fae05f7c18328ae5a5222
Arielle Murad <ariellem99@gmail.com>
2023-02-21 10:34:13 -0800

    get general info on players that match search

--- rel_bball_analytics/__init__.py
+++ rel_bball_analytics/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
 from dotenv import load_dotenv
-
-app = Flask(__name__)
-app.config.from_pyfile('settings.py')
+from flask import Flask, render_template
 
 load_dotenv()
 
-@app.route('/')
+app = Flask(__name__)
+app.config.from_pyfile("settings.py")
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
---
+++ rel_bball_analytics/players/utils.py
@@ -0,0 +1,14 @@
+import requests
+
+from rel_bball_analytics import app
+
+CURRENT_SEASON = 2022
+
+
+def get_data_from_api(endpoint: str, params: dict):
+    """Return JSON response from GET request to nba.api-sports.io"""
+    url = app.config["SPORTS_API_URL"] + endpoint
+    headers = {"x-rapidapi-key": app.config["SPORTS_API_KEY"]}
+
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()
