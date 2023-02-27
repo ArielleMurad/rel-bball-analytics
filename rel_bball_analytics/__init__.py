2db6b07e4a0df3dcedae13fcf365de6842a361ba
Arielle Murad <ariellem99@gmail.com>
2023-02-26 21:12:22 -0800

    [Tech Upgrade] - Configure db and create player model (#17)
    * configure db and create player model
    
    * update package version

--- rel_bball_analytics/__init__.py
+++ rel_bball_analytics/__init__.py
@@ -2,13 +2,20 @@ import logging
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from .settings import configure_db
 
 load_dotenv()
 
 app = Flask(__name__)
+app.app_context().push()
 
 app.config.from_pyfile("settings.py")
 app.logger.setLevel(logging.INFO)
+
+db = SQLAlchemy(app)
+configure_db(db=db)
 
 
 @app.route("/")
--- rel_bball_analytics/settings.py
+++ rel_bball_analytics/settings.py
@@ -1,4 +1,14 @@
 from os import environ
 
+from flask_sqlalchemy import SQLAlchemy
+
+SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+
 SPORTS_API_URL = environ.get("SPORTS_API_URL")
 SPORTS_API_KEY = environ.get("SPORTS_API_KEY")
+
+
+def configure_db(db: SQLAlchemy):
+    from .players.models import Player
+
+    db.create_all()
