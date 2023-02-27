7069ca4c801f4c6d7478b07b56c0a57042e2df0a
Arielle Murad <ariellem99@gmail.com>
2023-02-26 19:07:42 -0800

    configure db and create player model

---
+++ rel_bball_analytics/players/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Float, Integer, String
+
+from rel_bball_analytics import db
+
+
+class Player(db.Model):
+    __tablename__ = "players"
+
+    id = Column(Integer(), primary_key=True)
+    firstname = Column(String(32), nullable=False)
+    lastname = Column(String(32), nullable=False)
+    season = Column(Integer(), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now)
+    updated_at = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    birth_date = Column(String(16))
+    age = Column(Integer())
+    country = Column(String(32))
+    height_feet = Column(Integer())
+    height_inches = Column(Integer())
+    weight_pounds = Column(Integer())
+    jersey = Column(Integer())
+    is_active = Column(Boolean())
+    start_year = Column(Integer())
+    pro_years = Column(Integer())
+    college = Column(String(64))
+    team_id = Column(Integer())
+    team = Column(String(16))
+
+    games_played = Column(Integer())
+    points = Column(Float())
+    minutes_played = Column(Float())
+    field_goals_made = Column(Float())
+    field_goal_attempts = Column(Float())
+    field_goal_percentage = Column(Float())
+    three_points_made = Column(Float())
+    three_point_attempts = Column(Float())
+    three_point_percentage = Column(Float())
+    free_throws_made = Column(Float())
+    free_throw_attempts = Column(Float())
+    free_throw_percentage = Column(Float())
+    offensive_rebounds = Column(Float())
+    defensive_rebounds = Column(Float())
+    total_rebounds = Column(Float())
+    assists = Column(Float())
+    steals = Column(Float())
+    blocks = Column(Float())
+    turnovers = Column(Float())
+    personal_fouls = Column(Float())
