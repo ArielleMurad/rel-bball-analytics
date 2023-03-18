b623d17531e51e4f7859ec797425e35b292cd1ac
Arielle Murad <ariellem99@gmail.com>
2023-03-18 12:33:13 -0700

    fix players data_source and tests

--- rel_bball_analytics/players/tests/test_data_source.py
+++ rel_bball_analytics/players/tests/test_data_source.py
@@ -2,8 +2,10 @@ from unittest.mock import patch
 
 import pandas as pd
 
+from rel_bball_analytics.database import db
 from rel_bball_analytics.players.data_source import (
     clean_player_data,
+    fetch_player_data,
     filter_nba_players,
     get_player_matches,
 )
@@ -13,31 +15,69 @@ from .fixtures.api_responses import (
     players_nba,
     players_non_nba,
 )
+from .fixtures.database import reset_test_db, setup_test_db
 from .fixtures.dataframes import expected_player_matches
+from .fixtures.db_records import player_record, players_valid
 
 
 class TestGetPlayerMatches:
     """test suite for get_player_matches"""
 
-    @patch("rel_bball_analytics.players.player_info.get_data_from_api")
+    @patch("rel_bball_analytics.players.data_source.fetch_player_data")
+    def test_returns_existing_records_from_db(
+        self, fetch_player_data, reset_test_db, player_record
+    ):
+        db.session.add(player_record)
+        result = get_player_matches(name="Curry")
+
+        assert type(result) == pd.DataFrame
+        assert len(result) > 0
+        assert not fetch_player_data.called
+
+    @patch("rel_bball_analytics.players.data_source.save_records")
+    @patch("rel_bball_analytics.players.data_source.fetch_player_data")
+    def test_returns_data_from_api(
+        self, fetch_player_data, save_records, reset_test_db, players_valid
+    ):
+        fetch_player_data.return_value = pd.DataFrame(players_valid)
+        result = get_player_matches(name="Curry")
+
+        assert type(result) == pd.DataFrame
+        assert len(result) > 0
+        assert fetch_player_data.called
+        assert save_records.called
+
+    @patch("rel_bball_analytics.players.data_source.fetch_player_data")
+    def test_returns_none_if_no_matches_found(self, fetch_player_data):
+        fetch_player_data.return_value = None
+        result = get_player_matches(name="Curry")
+
+        assert result is None
+        assert fetch_player_data.called
+
+
+class TestFetchPlayerData:
+    """test suite for fetch_player_data"""
+
+    @patch("rel_bball_analytics.players.data_source.get_data_from_api")
     def test_returns_dataframe_for_valid_matches(self, get_data_from_api, players_nba):
         get_data_from_api.return_value = players_nba
-        results = get_player_matches(name="Curry")
+        results = fetch_player_data(name="Curry")
 
         assert type(results) == pd.DataFrame
         assert len(results) > 0
 
-    @patch("rel_bball_analytics.players.player_info.get_data_from_api")
+    @patch("rel_bball_analytics.players.data_source.get_data_from_api")
     def test_returns_none_if_no_matches(self, get_data_from_api, empty_response):
         get_data_from_api.return_value = empty_response
-        results = get_player_matches(name="Fake")
+        results = fetch_player_data(name="Fake")
 
         assert results is None
 
-    @patch("rel_bball_analytics.players.player_info.get_data_from_api")
+    @patch("rel_bball_analytics.players.data_source.get_data_from_api")
     def test_returns_none_if_no_nba_players(self, get_data_from_api, players_non_nba):
         get_data_from_api.return_value = players_non_nba
-        results = get_player_matches(name="Wembanyama")
+        results = fetch_player_data(name="Wembanyama")
 
         assert results is None
 
